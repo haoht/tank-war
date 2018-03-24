3ad9a27feeea5fec7a219b75c2b51f6bc769873b
Leo Qin <706046320@qq.com>
2018-03-24 16:20:45 +0800

    add tank direct

---
+++ src/v2_0/Members.java
@@ -0,0 +1,83 @@
+package v2_0;
+
+/**
+ * 坦克类
+ */
+class Tank {
+    // 横坐标
+    int x = 0;
+    // 纵坐标
+    int y = 0;
+    // 坦克方向 0上 1右 2下 3左
+    int direct = 0;
+    // 设置坦克速度
+    int speed = 1;
+
+    public Tank(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getDirect() {
+        return direct;
+    }
+
+    public void setDirect(int direct) {
+        this.direct = direct;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
+
+/**
+ * 我的坦克
+ */
+class MyTank extends Tank {
+
+    public MyTank(int x, int y) {
+        super(x, y);
+    }
+
+    // 坦克向上移动
+    public void moveUp() {
+        this.y -= this.speed;
+    }
+
+    // 坦克向右移动
+    public void moveRight() {
+        this.x += this.speed;
+    }
+
+    // 坦克向左移动
+    public void moveLeft() {
+        this.x -= this.speed;
+    }
+
+    // 坦克向下移动
+    public void moveDown() {
+        this.y += this.speed;
+    }
+
+}
--- src/v2_0/TankWar2.java
+++ src/v2_0/TankWar2.java
@@ -2,10 +2,13 @@ package v2_0;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 /**
- * 1.0
+ * 2.0
  * 1.画出坦克
+ * 2.我的坦克可以上下左右
  */
 public class TankWar2 extends JFrame {
     MyPanel mp = null;
@@ -17,6 +20,8 @@ public class TankWar2 extends JFrame {
     public TankWar2() throws HeadlessException {
         mp = new MyPanel();
         this.add(mp);
+        // 注册监听
+        this.addKeyListener(mp);
         this.setSize(400, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
@@ -26,19 +31,19 @@ public class TankWar2 extends JFrame {
 /**
  * 我的面板
  */
-class MyPanel extends JPanel {
+class MyPanel extends JPanel implements KeyListener {
     // 定义一个我的坦克
     MyTank myTank = null;
 
     public MyPanel() {
-        myTank = new MyTank(10, 10);
+        myTank = new MyTank(100, 100);
     }
 
     @Override
     public void paint(Graphics g) {
         super.paint(g);
         g.fillRect(0, 0, 400, 300);
-        this.drawTank(myTank.getX(), myTank.getY(), g, 0, 1);
+        this.drawTank(myTank.getX(), myTank.getY(), g, myTank.getDirect(), 1);
     }
 
     // 画出坦克函数
@@ -70,51 +75,79 @@ class MyPanel extends JPanel {
                 // 5.画出线
                 g.drawLine(x + 10, y + 15, x + 10, y);
                 break;
-
+            // 向右
+            case 1:
+                // 1、画出上边矩形
+                g.fill3DRect(x - 5, y, 30, 5, false);
+                // 1.画出下边矩形
+                g.fill3DRect(x - 5, y + 15, 30, 5, false);
+                // 3.画出中间矩形
+                g.fill3DRect(x, y + 5, 20, 10, false);
+                // 4.画出圆形
+                g.fillOval(x + 5, y + 5, 10, 10);
+                // 5.画出线
+                g.drawLine(x + 10, y + 10, x + 25, y + 10);
+                break;
+            // 向下
+            case 2:
+                // 画出坦克
+                g.fill3DRect(x, y, 5, 30, false);
+                // 1.画出右边矩形
+                g.fill3DRect(x + 15, y, 5, 30, false);
+                // 3.画出中间矩形
+                g.fill3DRect(x + 5, y + 5, 10, 20, false);
+                // 4.画出圆形
+                g.fillOval(x + 5, y + 10, 10, 10);
+                // 5.画出线
+                g.drawLine(x + 10, y + 15, x + 10, y + 30);
+                break;
+            // 向左
+            case 3:
+                // 画出坦克
+                // 1、画出上边矩形
+                g.fill3DRect(x - 5, y, 30, 5, false);
+                // 1.画出下边矩形
+                g.fill3DRect(x - 5, y + 15, 30, 5, false);
+                // 3.画出中间矩形
+                g.fill3DRect(x, y + 5, 20, 10, false);
+                // 4.画出圆形
+                g.fillOval(x + 5, y + 5, 10, 10);
+                // 5.画出线
+                g.drawLine(x + 10, y + 10, x - 5, y + 10);
+                break;
         }
     }
-}
 
-/**
- * 坦克类
- * x:横坐标
- * y:纵坐标
- */
-class Tank {
-    // 横坐标
-    int x = 0;
-    // 纵坐标
-    int y = 0;
+    @Override
+    public void keyTyped(KeyEvent e) {
 
-    public Tank(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 
-    public int getX() {
-        return x;
+    @Override // 按下a向左，s向下，w向上，d向右
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            // 设置坦克方向 默认向上
+            this.myTank.setDirect(0);
+            this.myTank.moveUp();
+        } else if (e.getKeyCode() == KeyEvent.VK_A) {
+            // 向左
+            this.myTank.setDirect(3);
+            this.myTank.moveLeft();
+        } else if (e.getKeyCode() == KeyEvent.VK_D) {
+            // 向右
+            this.myTank.setDirect(1);
+            this.myTank.moveRight();
+        } else if (e.getKeyCode() == KeyEvent.VK_S) {
+            // 向下
+            this.myTank.setDirect(2);
+            this.myTank.moveDown();
+        }
+        // 重新绘制panel
+        this.repaint();
     }
 
-    public void setX(int x) {
-        this.x = x;
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-}
-
-/**
- * 我的坦克
- */
-class MyTank extends Tank {
-
-    public MyTank(int x, int y) {
-        super(x, y);
-    }
-
 }
